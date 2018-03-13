abf87084f8f8358861c74d27992d257438d8d888
Nikoloz Pirtskhalava <npircxala@gmail.com>
2018-03-13 15:27:58 +0400

    Created Entity Song inside Core

---
+++ Music Player/Song.swift
@@ -0,0 +1,17 @@
+//
+//  Song.swift
+//  Music Player
+//
+//  Created by nikoloz on 13.03.18.
+//  Copyright © 2018 Mobility LLC. All rights reserved.
+//
+
+import Foundation
+
+
+struct Song {
+    var name: String?
+    var artistName: String?
+    var albumTitle: String?
+    var albumCoverUrl: String?
+}
