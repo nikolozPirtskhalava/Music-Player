4f961a894bcad2ce6da3aa76632e72e14c4755a0
Nikoloz Pirtskhalava <npircxala@gmail.com>
2018-03-13 16:24:37 +0400

    Started implementation of network layer

---
+++ Music Player/ApiSongsResponse.swift
@@ -0,0 +1,16 @@
+//
+//  ApiSongsResponse.swift
+//  Music Player
+//
+//  Created by nikoloz on 13.03.18.
+//  Copyright © 2018 Mobility LLC. All rights reserved.
+//
+
+import Foundation
+
+
+struct ApiSongsResponse {
+    var count: Int
+    var songs: [Song]
+    
+}
